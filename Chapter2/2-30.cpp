1d8df2047bf1937a56cc1a9c6b47c4a7ba1f5305
zzz <wzk1205434581@hotmail.com>
2019-07-22 10:12:43 +0800

    Create 2-30.cpp

---
+++ Chapter2/2-30.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int v2 = 0;           //顶层const
+    int v1 = v2;                
+    int *p1 = &v1, &r1 = v1;    //
+    const int *p2 = &v2, *conat p3 = &i, &r2 = v2;      //p2底层const p3既是顶层const也是底层const r2底层const
+}
