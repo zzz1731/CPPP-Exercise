7014f6373c10611a7575987e2b2e85391828c828
zzz <wzk1205434581@hotmail.com>
2019-07-22 11:02:35 +0800

    Update 2-30.cpp

--- Chapter2/2-30.cpp
+++ Chapter2/2-30.cpp
@@ -6,4 +6,5 @@ int main()
     int v1 = v2;                
     int *p1 = &v1, &r1 = v1;    //
     const int *p2 = &v2, *conat p3 = &i, &r2 = v2;      //p2底层const p3既是顶层const也是底层const r2底层const
+    return 0;
 }
