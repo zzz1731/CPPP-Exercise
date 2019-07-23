8513c8a949cd0ec78e927c2ffbb44211049e699f
zzz <wzk1205434581@hotmail.com>
2019-07-23 16:19:21 +0800

    Add 2-35.cpp

--- Chapter2/2-35.cpp
+++ Chapter2/2-35.cpp
@@ -1,11 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    const int i = 42;
-    auto j = i;
-    const auto &k = i;
-    auto *p = &i;
-    const auto j2 = i, &k2 = i;
-    return 0;
+    const int i = 42;           
+    auto j = i;             //j是int类型    
+    auto &k = i;            //k是整形常量的引用
+    auto *p = &i;           //p是指向整型常量的指针
+    const auto j2 = i, &k2 = i;     //j2是整形常量，k2是整形常量的引用
 }
