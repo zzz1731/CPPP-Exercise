fd88cf456e0e7e007c92a360d2a1aa68ad3e175b
zzz <wzk1205434581@hotmail.com>
2019-07-23 16:20:23 +0800

    Update 2-35.cpp

--- Chapter2/2-35.cpp
+++ Chapter2/2-35.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main()
 {
     const int i = 42;           
-    auto j = i;             //j是int类型    
-    auto &k = i;            //k是整形常量的引用
-    auto *p = &i;           //p是指向整型常量的指针
-    const auto j2 = i, &k2 = i;     //j2是整形常量，k2是整形常量的引用
+    auto j = i;                 //j是int类型    
+    auto &k = i;                //k是整形常量的引用
+    auto *p = &i;               //p是指向整型常量的指针
+    const auto j2 = i, &k2 = i; //j2是整形常量，k2是整形常量的引用
 }
