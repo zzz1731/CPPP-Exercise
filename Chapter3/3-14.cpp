47e67229439a87f09338c838aa2de8a32efda20a
zzz <wzk1205434581@hotmail.com>
2019-07-30 16:09:51 +0800

    Add 3-14.cpp,3-15.cpp

---
+++ Chapter3/3-14.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    vector<int> ivec;
+    while(cin>>n)
+    {
+        ivec.push_back(n);
+    }
+    return 0;
+}
---
+++ Chapter3/3-15.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    string s;
+    vector<string> svec;
+    while(cin>>s)
+    {
+        svec.push_back(s);
+    }
+    return 0;
+}
