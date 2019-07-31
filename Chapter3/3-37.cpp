3ed10e8a027a6abd6bc73312c68e270e7216187e
zzz <wzk1205434581@hotmail.com>
2019-07-31 16:38:41 +0800

    Add 3-37

---
+++ Chapter3/3-37.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	const char ca[] = { 'h','e','l','l','o' };
+	const char* cp = ca;
+	while (*cp)
+	{
+		cout << *cp << endl;
+		++cp;
+	}
+	return 0;
+}
+
+//程序的输出结果是hello与一些乱码
+//while（*cp)的判断条件是cp所指对象值是否为0
+//
