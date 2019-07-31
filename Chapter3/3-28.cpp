0f6e2821377b202230f2d24d4bfce8e42e7bcfef
zzz <wzk1205434581@hotmail.com>
2019-07-31 11:15:35 +0800

    Create 3-28.cpp

---
+++ Chapter3/3-28.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string sa[10];
+int ia[10];
+
+int main()
+{
+    string sa2[10];
+    int ia2[10];
+    return 0;
+}
+
+//sa含有10个空字符串
+//ia含有10个0
+//sa2含有10个空字符串
+//ia2含有10个0
+//Wed,Jul31,2019
