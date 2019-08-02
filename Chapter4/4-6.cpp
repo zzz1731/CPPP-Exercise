823528cba44ce5d86edb8c02d218af30599e1cba
zzz <wzk1205434581@hotmail.com>
2019-08-02 11:15:22 +0800

    Create 4-6.cpp

---
+++ Chapter4/4-6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	if (n % 2)
+	{
+		cout << "奇数" << endl;
+	}
+	else
+	{
+		cout << "偶数" << endl;
+	}
+	return 0;
+}
+
+//Fri,Aug2,2019
