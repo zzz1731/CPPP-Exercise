c1c6058fcb1d7ad3b9496a8f89cdb5cb00889aec
zzz <wzk1205434581@hotmail.com>
2019-08-06 17:18:36 +0800

    Update 5-20.cpp

--- Chapter5/5-20.cpp
+++ Chapter5/5-20.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 int main()
 {
-	vector<string> svec;
-	string s;
-	bool flag = true;
-	
-	while (cin >> s)
-	{
-		svec.push_back(s);
-		if (svec.size() > 1)
-		{
-			if (*(svec.end() - 1) == *(svec.end() - 2))
-			{
-				cout << *(svec.end() - 1);
-				flag = false;
-				break;
-			}
-		}
-	}
-	if (flag)
-	{
-		cout << "no repeat words" << endl;
-	}
-	return 0;
+    vector<string> svec;
+    string s;
+    bool flag = true;
+
+    while (cin >> s)
+    {
+        svec.push_back(s);
+        if (svec.size() > 1)
+        {
+            if (*(svec.end() - 1) == *(svec.end() - 2))
+            {
+                cout << *(svec.end() - 1) << endl;
+                flag = false;
+                break;
+            }
+        }
+    }
+    if (flag)
+    {
+        cout << "no repeat words" << endl;
+    }
+    return 0;
 }
 
 //编写一段程序，从标准输入中读取 string 对象的序列直到连续出现两个相同的单词或者所有单词都读完为止。
