e9e3169fd4a45c6b2e3823de91661fc307ecca8d
zzz <wzk1205434581@hotmail.com>
2019-08-14 09:17:17 +0800

    Create 6-27.cpp

---
+++ Chapter6/6-27.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <initializer_list>
+using namespace std;
+
+int sum(initializer_list<int>);
+
+int main()
+{
+	cout << sum({ 1,2,3,4,5,6,7 }) << endl;
+}
+
+int sum(initializer_list<int> il)
+{
+	int sum = 0;
+	for (const auto elem : il)
+	{
+		sum += elem;
+	}
+	return sum;
+}
+
+//编写一个函数，它的实参是 initializer_list<int> 类型的对象，函数的功能是计算列表中所有元素的和。
+//Wed,Aug14,2019
