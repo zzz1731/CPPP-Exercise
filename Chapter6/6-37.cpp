bf431b78d3cb18ed1d6daf03de72bb6f8745b7e9
zzz1731 <45326214+zzz1731@users.noreply.github.com>
2019-08-16 16:06:36 +0800

    Update 6-37.cpp

--- Chapter6/6-37.cpp
+++ Chapter6/6-37.cpp
@@ -12,11 +12,11 @@ using arrT = int[10];
 arrT* func();						//类型别名形式
 
 
-auto func()->int(*)[10];			//尾置返回类型形式
+auto func()->int(*)[10];			        //尾置返回类型形式
 
 
 int odd[] = { 0,1,2,3,4,5,6,7,8,9 };
-decltype(odd) *func();				//decltype表达式形式
+decltype(odd) *func();				        //decltype表达式形式
 
 
 int main()
