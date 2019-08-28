ebc7f4fbdd8aeeed590516e5eac067b39cbf00d9
zzz <wzk1205434581@hotmail.com>
2019-08-28 11:09:49 +0800

    Create 7-4.hpp

---
+++ Chapter7/7-4.hpp
@@ -0,0 +1,18 @@
+#pragma onece
+#ifndef _Person
+#define _Person
+
+#include <string>
+using std::string;
+
+class Person
+{
+private:
+    string strName;
+    string strAddress;
+};
+
+#endif
+
+//编写一个名为 Person 的类，使其表示人员的姓名和住址。使用string对象存放这些元素，接下来的练习将不断充实这个类的其他特征。
+//Wed,Aug28,2019
