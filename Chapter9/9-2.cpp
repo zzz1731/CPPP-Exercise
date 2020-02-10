b6d04fa117eca44c3ed22c65f24b3186f7bd3528
zzz <wzk1205434581@hotmail.com>
2020-02-10 16:51:23 +0800

    Create 9-2.cpp

---
+++ Chapter9/9-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <deque>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    list<deque<int>> list;
+}
+
+//定义一个 list 对象，其元素类型是 int 的 deque
+//Mon,Feb10,2020
