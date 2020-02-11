e988ce1dd997cf9d6601531715537ccc08b7b8f7
zzz <wzk1205434581@hotmail.com>
2020-02-11 09:27:23 +0800

    Create 9-4.cpp

---
+++ Chapter9/9-4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+bool search_vec(vector<int>::iterator begin, vector<int>::iterator end, int num)
+{
+    bool flag = false;
+    while (begin != end)
+    {
+        if (*begin != num)
+        {
+            ++begin;
+        }
+        else
+        {
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+}
+
+int main()
+{
+    vector<int> vec{1, 2, 3, 4, 5, 7, 8, 9};
+    cout << search_vec(vec.begin(), vec.end(), 7) << endl;
+    cout << search_vec(vec.begin(), vec.end(), 6) << endl;
+
+    return 0;
+}
+
+//编写函数，接受一对指向 vector<int> 的迭代器和一个 int 值。在两个迭代器指定的范围中查找给定的值，返回一个布尔值来指出是否找到。
+//Tue,Feb11,2020
